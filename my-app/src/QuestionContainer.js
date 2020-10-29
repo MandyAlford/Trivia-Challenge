043f714a77e4d80267eb9339a4757d9589ed9c59
Mandy Alford <mandyalford@gmail.com>
2020-10-28 21:59:28 -0400

    Set questions for the round in state and display first question on page

--- my-app/src/QuestionContainer.js
+++ my-app/src/QuestionContainer.js
@@ -1,26 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import data from './data';
 
 function QuestionContainer() {
+       
+    function getQuestions() {
+        let result = [];
 
-    const getQuestions = () => {
-        let result = []
-        
-        while(result.length < 10) {
-            let index = Math.floor(Math.random() * data.length)
+        while (result.length < 10) {
+            let index = Math.floor(Math.random() * data.length);
 
-            if(!result.includes(index)) {
-                result.push(index)
+            if (!result.includes(index)) {
+                result.push(index);
             }
         }
-        return result.map(index => {
-            return data[index]
-        })
+        let questionsData = result.map(index => {
+            return data[index];
+        });
+        return questionsData;
     }
+
+    const [questions, setQuestionBank] = useState(getQuestions());
+
+    // const updateCurrentQuestion = ()=> {
+    //     return setCurrentQuestion(currentQuestion = questions[counter])
+    // }
+
+
+    const [counter, setCounter] = useState(0);
+    // const [currentQuestion, setCurrentQuestion] = useState(questions[counter]);
+
+    // useEffect(() => {
+    //     getQuestions()
+    // }, [])
 
     return (
         <div className='QuestionContainer'>
-            <h1>First Question</h1>
+            <h1>{questions[counter].question}</h1>
         </div>
     );
 }
